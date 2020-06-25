dc325ef06eafc8684e8b50ba20880bc79919b725
Nero951 <zhlwd103511@gmail.com>
2020-06-25 22:33:45 +0800

    20200625

---
+++ 20200625/Project1/Project1/C1.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//运行结果67
+//int cnt = 0;
+//int fib(int n)
+//{
+//	cnt++;
+//	if (n == 0)
+//		return 1;
+//	else if (n == 1)
+//		return 2;
+//	else
+//		return fib(n - 1) + fib(n - 2);
+//}
+//void main()
+//{
+//	fib(8);
+//	printf("%d", cnt);
+//}
+
